93ad930c419543eb7a7ace7033948080c3fd28dc
xtaC <63303414+ecstaC@users.noreply.github.com>
2024-09-21 03:00:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function encryptText() {
-    let inputText = document.getElementById('inputText').value;
-    let encryptedText = btoa(inputText); // Base64 encoding
-    document.getElementById('encryptedText').innerText = encryptedText;
+  let inputText = document.getElementById('inputText').value;
+  let encryptedText = btoa(inputText); // Base64 encoding
+  document.getElementById('encryptedText').innerText = encryptedText;
+}
+
+function decryptText() {
+  let inputText = document.getElementById('inputText').value;
+  try {
+    let decryptedText = atob(inputText); // Base64 decoding
+    document.getElementById('encryptedText').innerText = decryptedText;
+  } catch (e) {
+    document.getElementById('encryptedText').innerText = "Invalid input for decryption!";
   }
+}

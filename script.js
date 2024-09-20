78c1814e51438fe81c0ef2eb322041e2cdb4eb64
xtaC <63303414+ecstaC@users.noreply.github.com>
2024-09-21 03:03:40 +0530

    Update script.js
    outputtext

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function encryptText() {
   let inputText = document.getElementById('inputText').value;
   let encryptedText = btoa(inputText); // Base64 encoding
-  document.getElementById('encryptedText').innerText = encryptedText;
+  document.getElementById('outputText').innerText = encryptedText;
 }
 
 function decryptText() {
   let inputText = document.getElementById('inputText').value;
   try {
     let decryptedText = atob(inputText); // Base64 decoding
-    document.getElementById('encryptedText').innerText = decryptedText;
+    document.getElementById('outputText').innerText = decryptedText;
   } catch (e) {
-    document.getElementById('encryptedText').innerText = "Invalid input for decryption!";
+    document.getElementById('outputText').innerText = "Invalid input for decryption!";
   }
 }
